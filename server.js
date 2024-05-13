cd475a557d715bc96e184d4b98ebf54b2d75a7b4
Adithya-S-Nair <adithyasnair00@gmail.com>
2024-05-13 08:12:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 5000; // You can change the port as needed
+
+// Define the route for /helloworld
+app.get('/helloworld', (req, res) => {
+    res.status(200).json({ message: 'Hello, world!' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+});
